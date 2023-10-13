ebf6fb9609df4ac00354a46ddef9d6130b08a581
Sepecter <1170724164@qq.com>
2023-10-13 17:39:13 +0800

    register

--- server/models.py
+++ server/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class User_Info(models.Model):  # 用户信息
     username = models.TextField(default='')
     password = models.TextField(max_length=32, default='')
-    token = models.TextField(max_length=32,default='')
+    token = models.TextField(max_length=64,default='')
--- server/views.py
+++ server/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework import exceptions
+from server import models
+from django.http import JsonResponse
+import base64
+
+class Register(APIView):
+
+    def post(self, request):
+        ret = {}
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not username or not password:
+            ret['status_code'] = 404
+            ret['msg'] = '请求参数错误'
+            return JsonResponse(ret)
+        user = models.User_Info.objects.filter(username=username).first()
+        if user:
+            ret['status_code'] = 404
+            ret['msg'] = '用户已注册'
+            return JsonResponse(ret)
+        models.User_Info.objects.create(username=username, password=password, token=token)
+        ret['status_code'] = 200
+        ret['msg'] = '注册成功'
+        return JsonResponse(ret)
