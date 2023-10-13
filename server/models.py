d6054141e325cc34146129fdb3f63f136c0ae2af
Sepecter <1170724164@qq.com>
2023-10-13 17:27:35 +0800

    models:UserInfo

--- server/models.py
+++ server/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User_Info(models.Model):  # 用户信息
+    username = models.TextField(default='')
+    password = models.TextField(max_length=32, default='')
+    token = models.TextField(max_length=32,default='')
