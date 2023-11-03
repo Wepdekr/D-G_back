ebd2ea48c17e9e2af5ee9dd0b9bd725d2d4307b4
Sepecter <1170724164@qq.com>
2023-11-03 15:41:03 +0800

    url:login

--- D_G/urls.py
+++ D_G/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from server import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'login', views.Login.as_view()),
 ]
